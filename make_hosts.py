20633893b6301b2a934379b5dba6e60a13c1bc4c
JH <29008730+idunno101@users.noreply.github.com>
2021-05-23 03:52:16 +0000

    Remove template

--- make_hosts.py
+++ make_hosts.py
@@ -1,8 +1,3 @@
-# entering the file names
-firstfile = "1.txt" 
-# input("Enter the name of first file ")
-secondfile = "2.txt"
-# input("Enter the name of second file ")
 import os
 
 firstfile = "./Domains/" + os.environ['OUTPUT_FILE']
@@ -15,6 +10,5 @@ for line in f2:
     if line != 'null\n':
         f1.write("0.0.0.0  " + line)
  
-# closing the files
 f1.close()
 f2.close()
