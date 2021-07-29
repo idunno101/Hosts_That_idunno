b0fe7a357f48da705cd9be6669f45fb85de31b9c
idunno101 <29008730+idunno101@users.noreply.github.com>
2021-07-28 22:06:03 -0400

    Get csv file and remove jq for all other workflows

--- make_hosts.py
+++ make_hosts.py
@@ -7,7 +7,7 @@ f1 = open(firstfile, 'w')
 f2 = open(secondfile, 'r')
     
 for line in f2:
-    if line != 'null\n':
+    if line != 'null\n' && line != "indicator":
         f1.write("0.0.0.0  " + line)
  
 f1.close()
