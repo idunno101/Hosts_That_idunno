5fb3d15ca9e5df1296a505eecf35364960fe63ae
JH <29008730+idunno101@users.noreply.github.com>
2021-07-28 23:58:31 -0400

    Fix python 'and'

--- make_hosts.py
+++ make_hosts.py
@@ -7,7 +7,7 @@ f1 = open(firstfile, 'w')
 f2 = open(secondfile, 'r')
     
 for line in f2:
-    if line != 'null\n' && line != "indicator":
+    if line != 'null\n' and line != "indicator":
         f1.write("0.0.0.0  " + line)
  
 f1.close()
