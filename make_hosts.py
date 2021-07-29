e38f31dc99551202b40ced35965377c57c3c15f7
JH <29008730+idunno101@users.noreply.github.com>
2021-07-29 00:12:07 -0400

    Try to remove 'indicator' form Domains

--- make_hosts.py
+++ make_hosts.py
@@ -7,7 +7,7 @@ f1 = open(firstfile, 'w')
 f2 = open(secondfile, 'r')
     
 for line in f2:
-    if line != 'null\n' and line != "indicator":
+    if line != 'null\n' and line != "indicator\n":
         f1.write("0.0.0.0  " + line)
  
 f1.close()
